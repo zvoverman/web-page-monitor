1a49d895dac2bd83e0ad38e3c09278bcaa0e4104
zvoverman <zvoverman@gmail.com>
2024-02-08 18:36:54 -0700

    server and client simplified

--- server/app.js
+++ server/app.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
+const puppeteer = require('puppeteer')
 
 const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 8080;
-const monitorRoutes = require('./routes/monitorRoutes');
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Mount monitor routes
-app.use('/api', monitorRoutes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+app.post('/api/monitor/:url' , (req, res) => {
+  const url = decodeURIComponent(req.params.url)
+
+  monitor(url)
+
+  res.status(200).send("monitoring " + url)
+})
+
+async function monitor(url) {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto(url);
+
+  await browser.close()
+
+  console.log("closed browser")
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Internal Server Error' });
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
 });
 
 // Start the server
